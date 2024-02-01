f581f53f2228c32c91908b2038f34393f3bb80a0
Thomas Kientz <thomas@kientz.net>
2024-02-01 15:15:00 +0100

    Update dependencies, remove format from CLI

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# ECB-scraper
-Scraper for ECB press conferences
+<h1 align="center">ECB Scraper</h1>
+
+<p align="center">
+    <img alt="PyPI - Python Version" src="https://img.shields.io/pypi/pyversions/ecb-scraper">
+    <br>
+    <a href="https://pypi.org/project/ecb-scraper/"><img src="https://badge.fury.io/py/ecb-scraper.svg" alt="PyPI version"></a>
+    <img src="https://img.shields.io/badge/license-MIT-green.svg" alt="License">
+</p>
+
+ECB Scraper is a Python package for scraping European Central Bank press conference transcripts. It allows for easy extraction of press conference details including dates and URLs, offering a streamlined way to access ECB statements and Q&A sessions over the years.
+
+## Installation
+
+Install `ecb-scraper` via pip:
+
+```bash
+pip install ecb-scraper
+```
+
+## Usage
+
+### CLI
+
+The ECB Scraper also provides a Command Line Interface (CLI) for fetching ECB press conferences without needing to write any Python code. Here's how you can use it:
+
+Arguments
+- --start-year: Specify the start year for fetching the conferences. If not provided, the script will use 1998.
+- --end-year: Specify the end year for fetching the conferences. If not provided, the current year is used.
+- --output-file: Required. The path to the file where the fetched data will be saved (CSV or JSON).
+
+```bash
+ecb-scraper --start-year 2019 --end-year 2020 --output-file conferences.json
+```
+
+### Python
+
+The ECB Scraper can also be used as a Python package. Here's how you can use it:
+
+```python
+from ecb_scraper import get_all_conferences
+
+# DataFrame with columns ['date', 'title', 'link', 'text']
+conferences = get_all_conferences(start_year=2005, end_year=2020)
+```
--- ecb_scraper/cli.py
+++ ecb_scraper/cli.py
@@ -6,7 +6,7 @@ from .scraper import get_all_conferences
 from .config import MIN_YEAR
 
 
-def save_data(df: pd.DataFrame, output_file: str, format: str) -> None:
+def save_data(df: pd.DataFrame, output_file: str) -> None:
     """
     Save the DataFrame in the specified format.
 
@@ -16,15 +16,15 @@ def save_data(df: pd.DataFrame, output_file: str, format: str) -> None:
         The DataFrame to save.
     output_file : str
         The path to the output file.
-    format : str
-        The format in which to save the DataFrame ('csv' or 'json').
     """
-    if format == "csv":
+    format_ = output_file.split(".")[-1]
+
+    if format_.lower() == "csv":
         df.to_csv(output_file, index=False)
-    elif format == "json":
+    elif format_.lower() == "json":
         df.to_json(output_file, orient="records")
     else:
-        raise ValueError(f"Unsupported format: {format}")
+        raise ValueError(f"Unsupported format: {format_}")
 
 
 def main():
@@ -32,9 +32,8 @@ def main():
     parser = argparse.ArgumentParser(description="Fetch ECB press conferences and save them in a specified format.")
     parser.add_argument("--start-year", type=int, default=None, help="The start year for fetching conferences.")
     parser.add_argument("--end-year", type=int, default=None, help="The end year for fetching conferences.")
-    parser.add_argument("--output-file", type=str, required=True, help="The path to the output file.")
     parser.add_argument(
-        "--format", type=str, choices=["csv", "json"], default="csv", help="The output file format (csv or json)."
+        "--output-file", type=str, required=True, help="The path to the output file. Format must be CSV or JSON."
     )
 
     args = parser.parse_args()
@@ -44,9 +43,9 @@ def main():
 
     conferences_df = get_all_conferences(start_year=args.start_year, end_year=args.end_year)
 
-    save_data(conferences_df, args.output_file, args.format)
+    save_data(conferences_df, args.output_file)
 
-    print(f"Data saved to {args.output_file} in {args.format} format.")
+    print(f"Data saved to {args.output_file} format.")
 
 
 if __name__ == "__main__":
